9f07b02b3cc25c9c8de489d3e81f9d57dbff6217
Serhii Podobnyi <serhii.podobnyi@nixsolutions.com>
2021-11-23 18:55:23 +0200

    Сreated branch develop and empty project

---
+++ WeatherAppSP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppSP
+//
+//  Created by Сергей Александрович on 23.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
